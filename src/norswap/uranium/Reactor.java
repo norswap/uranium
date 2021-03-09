fb8a58684e196ce90b5e67f52d9ef4cf59fc7b4b
Nicolas "Norswap" Laurent <norswap@gmail.com>
2021-03-09 14:02:10 +0100

    wrap exceptions thrown when running rules

--- src/norswap/uranium/Reactor.java
+++ src/norswap/uranium/Reactor.java
@@ -222,7 +222,13 @@ public class Reactor
     {
         while (!queue.isEmpty()) {
             Rule rule = queue.removeFirst();
-            rule.run();
+
+            try {
+                rule.run();
+            } catch (RuntimeException e) {
+                throw new RuntimeException("exception while running: " + rule, e);
+            }
+
             for (int i = 0; i < rule.exports.length; ++i) {
                 Attribute attribute = rule.exports[i];
                 Object value = rule.exportValues[i];
